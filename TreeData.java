073a0a8cb1e72aa964dfc4bb3fbd51c86fdc54e3
Donia Tung <doniatung1@Donias-MacBook-Pro.local>
2018-10-14 14:27:04 -0400

    added TreeData

---
+++ TreeData.java
@@ -0,0 +1,27 @@
+
+public class TreeData {
+	private char key;
+	private int value;
+	
+	public TreeData(char k, int v) {
+		key = k;
+		value = v;
+	}
+
+	public char getKey() {
+		return key;
+	}
+
+	public void setKey(char key) {
+		this.key = key;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+}
